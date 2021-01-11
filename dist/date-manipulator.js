e811b3512cf9324bc7efd0c25edd3efb31e2a1b4
vinaykumargb <vinaykumargb3@gmail.com>
2021-01-11 20:02:59 +0530

    Bug fix and improvements

--- dist/date-manipulator.js
+++ dist/date-manipulator.js
@@ -2,7 +2,7 @@
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.dateM = factory());
+  (global = global || self, global.dateM = factory());
 }(this, (function () { 'use strict';
 
   function _typeof(obj) {
--- test/test.js
+++ test/test.js
@@ -6,9 +6,10 @@ var d = new dateM(2020, 1, 10, {
 });
 
 function test(expected, actual) {
-  assert.equal(expected, actual);
+  assert.deepEqual(expected, actual);
   console.log('\033[34m✓', expected);
 }
-
-test('10 1 2020 12:0 AM', d.current('dd mm yyyy hh:mm'), '10 1 2020 12:0 AM');
+																				
+test('10 1 2020 12:0 AM', d.current('dd mm yyyy hh:mm'));
 test('366', dateM.timePassed(new dateM(2020, 0, 1), new dateM(2021, 0, 1)));
+		
